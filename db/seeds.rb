77a857da360639c33250287d760ae14e734d15dc
Bastien Perez Assael <perez.bas@gmail.com>
2022-09-16 15:28:03 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Article.destroy_all
+User.destroy_all
+
+user1 = User.new(first_name: "Silvia", last_name: "Pasini", email: "silvia@silvia.com", password: "xxxxxxxx")
+user1.save!
+
+user2 = User.new(first_name: "Olivier", last_name: "Barthelemy", email: "olivier@olivier.com", password: "xxxxxxxx")
+user2.save!
+
+user3 = User.new(first_name: "Alice", last_name: "Daubrée", email: "alice@alice.com", password: "xxxxxxxx")
+user3.save!
+
+user4 = User.new(first_name: "Bastien", last_name: "Perez Assael", email: "bastien@bastien.com", password: "xxxxxxxx")
+user4.save!
+
+
+users = [user1, user2, user3, user4]
+
+categories = ["Business", "Culture", "Entertainement", "Politics", "Environment"]
+
+
+50.times do
+  Article.new(title: Faker::Book.title,
+              author: Faker::Name.name,
+              category: categories.sample,
+              content: Faker::Lorem.paragraph(sentence_count: 10),
+              url: Faker::Internet.url,
+              user: users.sample)
+end
